2e1da95d8eee0c56db6c4f91fa3fb072286f1d44
0xmaximize <rapagambler@gmail.com>
2024-03-26 06:34:26 +0700

    tesar

--- sections/Buy.jsx
+++ sections/Buy.jsx
@@ -327,7 +327,7 @@ const Buy = () => {
           </CardHeader>
           <div className="mx-5">
           <Progress
-          value={15.8}
+          value={18.4}
           maxValue={100}
             classNames={{
               base:'py-4',
@@ -338,7 +338,7 @@ const Buy = () => {
           </div>
 
           <div className="flex justify-between px-5 mt-[-4px]">
-            <h4 className="font-bold text-sm text-orange-200">15.8 SOL</h4>
+            <h4 className="font-bold text-sm text-orange-200">18.4 SOL</h4>
             <h4 className="font-bold text-sm text-orange-200">100 SOL</h4>
           </div>
           <CardBody>
