35d484516e1421932bbcffedbecfc240cb6497b7
0xmaximize <135501343+0xmaximize@users.noreply.github.com>
2024-04-09 22:14:52 +0700

    Update Buy.jsx

--- sections/Buy.jsx
+++ sections/Buy.jsx
@@ -332,7 +332,7 @@ const Buy = () => {
           </CardHeader>
           <div className="mx-5">
           <Progress
-          value={89.5}
+          value={91}
           maxValue={100}
             classNames={{
               base:'py-4',
@@ -343,7 +343,7 @@ const Buy = () => {
           </div>
 
           <div className="flex justify-between px-5 mt-[-4px]">
-            <h4 className="font-bold text-sm text-orange-200">185.4 SOL</h4>
+            <h4 className="font-bold text-sm text-orange-200">190.4 SOL</h4>
             <h4 className="font-bold text-sm text-orange-200">250 SOL</h4>
           </div>
 
