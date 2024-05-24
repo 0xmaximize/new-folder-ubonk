b34cefbda889f205fc89f8233829a58b06d50619
0xmaximize <135501343+0xmaximize@users.noreply.github.com>
2024-05-25 03:44:29 +0700

    Update Buy.jsx

--- sections/Buy.jsx
+++ sections/Buy.jsx
@@ -323,7 +323,7 @@ const Buy = () => {
        <div className="grid gap-4">
       <Card className="bg-gray-800 lg:w-[400px] border-gray-800">
           <CardHeader className="flex justify-center items-center">
-          <DynamicTicker endTime="2024-05-23T19:00:00Z" />
+          <DynamicTicker endTime="2024-05-26T19:00:00Z" />
           
           </CardHeader>
           <div className="mx-5">
